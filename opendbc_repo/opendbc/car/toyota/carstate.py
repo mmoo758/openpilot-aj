4d9b63f27d16e268d695fe87acd6796560d155f9
mmoo758 <yccao94@gmail.com>
2025-08-29 11:37:21 +0800

    Update carstate.py

--- opendbc_repo/opendbc/car/toyota/carstate.py
+++ opendbc_repo/opendbc/car/toyota/carstate.py
@@ -91,7 +91,7 @@ class CarState(CarStateBase):
     )
     ret.vEgoRaw = float(np.mean([ret.wheelSpeeds.fl, ret.wheelSpeeds.fr, ret.wheelSpeeds.rl, ret.wheelSpeeds.rr]))
     ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
-    ret.vEgoCluster = ret.vEgo * 1.015  # minimum of all the cars
+    ret.vEgoCluster = ret.vEgo * 1.055  # minimum of all the cars
 
     ret.standstill = abs(ret.vEgoRaw) < 1e-3
 
