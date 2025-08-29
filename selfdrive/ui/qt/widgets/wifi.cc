94fe8db900ef0521afabf25a7283c8fb9617cd26
mmoo758 <yccao94@gmail.com>
2025-08-29 11:56:08 +0800

    Update wifi.cc

--- selfdrive/ui/qt/widgets/wifi.cc
+++ selfdrive/ui/qt/widgets/wifi.cc
@@ -11,7 +11,7 @@ WiFiPromptWidget::WiFiPromptWidget(QWidget *parent) : QFrame(parent) {
   main_layout->setContentsMargins(56, 40, 56, 40);
   main_layout->setSpacing(42);  
   
-  QLabel *title = new QLabel(tr("<span style='font-family: \"Noto Color Emoji\";'>🔥</span> Firehose Mode <span style='font-family: Noto Color Emoji;'>🔥</span>"));
+  QLabel *title = new QLabel(tr("训练数据上传模式"));
   title->setStyleSheet("font-size: 64px; font-weight: 500;");
   main_layout->addWidget(title);
 
